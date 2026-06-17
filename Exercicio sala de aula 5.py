397c8f5f9012ba081feb3083ada540e25d7dd933
taleseugenio17-tech <taleseugenio17@gmail.com>
2026-06-17 15:26:15 -0300

    Add files via upload

---
+++ Exercicio sala de aula 5.py
@@ -0,0 +1,42 @@
+# Exercicios de sala
+# print('Welcome to Python Class')
+
+# a = 8.8
+# b = 10.5
+# print(type(a))
+# resultado = (a * b)
+# print(resultado)
+
+# g = 10
+# h = 5
+# results = (10 / 5)
+# print(results)
+
+# c = 9
+# d = 10
+# results = c > d
+# compare = d == c
+# different = c != d
+# print(results, compare, different)
+
+# v = 10
+# N = 5
+# results = (v // 2)
+# print(results)
+
+# I = 4
+# results = (4**2)
+# print(results)
+
+Nome = input('Type your name:')
+email = input('type your email:')
+Idade = int(input('Type your age:'))
+Cidade = ('São Paulo')
+Graduação = ('Mechanical Engineer')
+Estado_Civil = ('Casado')
+# print('{} {} {} {} {} {}'. format(Nome, email, Idade, Cidade, Graduação, Estado_Civil))
+print(f'Meu nome é {Nome} meu email {email} minha idade {Idade}')
+print(f'moro em {Cidade} e sou formado em {Graduação} e sou {Estado_Civil}') 
+
+
+
