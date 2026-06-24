75b30b11c154107aef51e0a79b4730d08f8019c8
taleseugenio17-tech <taleseugenio17@gmail.com>
2026-06-24 17:40:30 -0300

    Add files via upload

---
+++ Match case.py
@@ -0,0 +1,13 @@
+n = input('Texto')
+match n:
+    case '':
+        print('Variáve Vazia')
+    case_:
+        print('Variável cheia')
+
+
+Idade = int(input('>>'))
+match Idade: 
+    case Idade  if Idade <=12:
+        print('Criança')
+    case Idade if Idade 
