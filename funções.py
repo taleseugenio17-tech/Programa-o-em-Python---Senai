92c32d9a2789591d274fd3dfab790b1a6dfb3bee
taleseugenio17-tech <taleseugenio17@gmail.com>
2026-06-26 17:58:42 -0300

    Add files via upload

---
+++ funções.py
@@ -0,0 +1,79 @@
+#def é a palavra da função. Vindas da propria linguagem ex: sum, print, len.
+# ou pode vir do modulos ex.: randon radnint
+#sem começamos com a palavra def
+# se não forem invocadas elas não funcionan
+#para invocar basta chamar pelo nome da funçaõ.
+
+# def valor_hora(carga, salario):
+#     return salario / carga
+
+# def extra_50(valor_hora):
+#     return valor_hora * 1.5
+
+# def salario_total(quantidade_extra, salario, extra_50_):
+#     valor_q = quantidade_extra * extra_50_
+#     total   =  valor_q + salario
+#     return total, valor_q
+
+
+# def sistema_RH():
+#     carga  = float(input('Carga: '))
+#     salario = float(input('Salário: '))
+#     v_h  = valor_hora(carga, salario)
+#     print(f'Valor hora: , {v_h:.2f}')
+#     print('------------- ')
+#     print('HORA EXTRA 50% ')
+#     ex_50 = extra_50(v_h)
+#     print(f'{ex_50:.2f}')
+#     print('------------- ')
+#     quantidade = float(input('Extra quantidade: '))
+#     sal = salario_total(quantidade, salario, ex_50 )
+#     print('R$', round(sal[0],2), "Total hora extra",  round(sal[1],2))
+
+# sistema_RH()
+
+# def Valor_hora(carga, salário)
+    
+
+
+# def comparar_2_numeros ():
+#     n1 = float(input('Digite um número: ' ))
+#     n2 = float(input('Digite um número: ' ))
+   
+#     if n1 % 2 == 0 and n2 % 2 == 0:
+#         print(f'O {n1} é par {n2} é par')
+
+#     elif n1 % 2 == 0 and n2 % 2 != 0:
+#         print(f'O {n1} é par {n2} é impar')
+
+#     elif n1 % 2 != 0 and n2 % 2 == 0:
+#         print(f'O {n1} é impar {n2} é par')
+#     else:
+#         print('Ambos os números são impares')
+    
+# comparar_2_numeros ()
+
+
+#calculadora
+
+def calculadora ()
+    n1 = float(input('Digite o primeiro número: '))
+    n2 = float(input('Digite o segundo número: '))
+    def soma ()
+        soma = n1 + n2
+    
+    def sub ()
+        sub = n1 - n2
+    
+    def mul ()
+        mult = n1
+
+
+
+    
+
+
+   
+
+       
+    
