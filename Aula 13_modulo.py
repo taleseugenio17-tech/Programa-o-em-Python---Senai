5eb8affcfc84e829b3b1bf3d73108f1109c238e7
taleseugenio17-tech <taleseugenio17@gmail.com>
2026-07-02 17:46:40 -0300

    Add files via upload

---
+++ Aula 13_modulo.py
@@ -0,0 +1,59 @@
+import random as rm
+
+#Exercicio 1 - Criar numeros aleatórios no intervalo de 5 a 10.
+
+# def aleatório():
+#     n = random.randint(a, z)
+#     return(n)
+
+
+#Exercicio 2 - Criar 3 numeros aleatórois
+# def numeros aleatórios:
+#     for i in range (3):
+#         num = random. randint(1, 1000)
+#         print(num)
+
+# #Exercicio 3 numeros aleatórios
+# For i in range (10, 30, 2):
+#     num = random.randint()
+#     print(num)
+
+
+#Exercicio 4 - COntagem regressive
+# from time import sleep
+
+# for number in range (10, -1, -1):
+#     print(number)
+#     sleep(1)
+# print('Foguete lançado')
+
+
+#Exercício 5 - SOma de números pares.
+# n = int(input('Digite um numero'))
+# soma = 0
+# for i in range (2, n + 1):
+#     if i % 2 == 0:
+#         som = soma + i
+# print(f' O numero é: {som}')
+
+
+# n = int(input('Digite um numero:'))
+
+def tabuada(numero):
+    for i in range (11):
+        calculo = i * numero
+        print({calculo})
+
+
+#Exercicio 7 - Criar números impares reversos
+
+
+
+
+
+
+
+
+
+
+
